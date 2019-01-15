923408e2e2500496872691262d977cbc14a763b0
Alvaro Garcia <derecerca@gmail.com>
2019-01-15 20:49:23 +0100

    Document how the program can be used

--- src/main/java/com/codurance/training/tasks/TaskList.java
+++ src/main/java/com/codurance/training/tasks/TaskList.java
@@ -211,9 +211,11 @@ public final class TaskList implements Runnable {
         out.println("Commands:");
         out.println("  show");
         out.println("  add project <project name>");
-        out.println("  add task <project name> <task description>");
+        out.println("  add task <project name> [<id>:] <task description>");
         out.println("  check <task ID>");
         out.println("  uncheck <task ID>");
+        out.println("  delete <task ID>");
+        out.println("  today");
         out.println();
     }
 
